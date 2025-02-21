4e40168a626674dc396a2cee7656825914ce80aa
ChienDC <144684564+ChienDC@users.noreply.github.com>
2025-02-21 22:36:28 +0700

    Revert "admin template"

--- DiamondJewelry/Program.cs
+++ DiamondJewelry/Program.cs
@@ -1,5 +1,4 @@
 using DiamondJewelry.Data;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,57 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Lấy Connection String từ appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-  .AddEntityFrameworkStores<AspnetCoreStarterMvcContext>()
-  .AddDefaultTokenProviders();
-
-
 // Thêm DbContext
-builder.Services.AddDbContext<AspnetCoreStarterMvcContext>(options =>
+builder.Services.AddDbContext<JewelryDbContext>(options =>
     options.UseSqlServer(connectionString));
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.ConfigureApplicationCookie(options =>
-{
-  options.LoginPath = "/Auth/Login"; // Account/Login <- default
-  options.AccessDeniedPath = "/Auth/AccessDenied"; // Trang 403, đăng nhập rồi nhưng không phải admin hoặc ...
-  options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-});
-
-builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
 var app = builder.Build();
-
-var scope = app.Services.CreateScope();
-// tạo mới role
-var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-// tạo mới user
-var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-
-if (!await roleManager.RoleExistsAsync("Admin"))
-{
-  await roleManager.CreateAsync(new IdentityRole("Admin"));
-}
-
-var adminUser = new IdentityUser
-{
-  UserName = "diamond@example.com",
-  Email = "diamond@example.com",
-  EmailConfirmed = true
-};
-
-if (await userManager.FindByEmailAsync(adminUser.Email) == null)
-{
-  var result = await userManager.CreateAsync(adminUser, "Admin@123");
-  if (result.Succeeded)
-  {
-    await userManager.AddToRoleAsync(adminUser, "Admin");
-  }
-}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -70,18 +25,14 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
 
 app.MapControllerRoute(
-  name: "default",
-  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
-app.MapControllerRoute(
-  name: "default",
-  pattern: "{controller=Home}/{action=Index}/{id?}");
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
 
 
 app.Run();
